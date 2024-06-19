8d9c6f89059660b85f87c6305e9f2c3a02da9e9a
Kirill Dvoretsky <shortykofficial@gmail.com>
2024-06-19 17:20:04 +0300

    added background information on how the script works

---
+++ hexlet_code/scripts/gendiff.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+parser.add_argument("first_file")
+parser.add_argument("second_file")
+args = parser.parse_args()
