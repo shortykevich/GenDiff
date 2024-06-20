b7a25235a0edcef306d3e029b18539f1dbe4a249
Kirill Dvoretsky <shortykofficial@gmail.com>
2024-06-20 04:05:30 +0300

    Made gendiff function and updated README with installation and basic use cases demonstation

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
-import argparse
+import json
 
 
-parser = argparse.ArgumentParser(
-    description='Compares two configuration files and shows a difference.'
-)
-parser.add_argument("first_file")
-parser.add_argument("second_file")
-parser.add_argument("-f", "--format", help="set format of output")
-args = parser.parse_args()
+def open_file(file_path: str) -> tuple:
+    return json.load(open(file_path))
+
+
+def generate_diff(file1_path: str,
+                  file2_path: str) -> str:
+    file1, file2 = open_file(file1_path), open_file(file2_path)
+    files_union = file1 | file2
+    sorted_files_union = dict(sorted(files_union.items()))
+    result = []
+    for key, val in sorted_files_union.items():
+        is_key_in_file1, is_key_in_file2 = key in file1, key in file2
+        if is_key_in_file2 and not is_key_in_file1:
+            result.append(f"+ {key}: {val}")
+        elif is_key_in_file1 and not is_key_in_file2:
+            result.append(f"- {key}: {val}")
+        elif is_key_in_file1 and file1[key] == val:
+            result.append(f"  {key}: {val}")
+        elif is_key_in_file1 and file1[key] != val:
+            result.append(f"- {key}: {file1[key]}\n" +
+                          f"+ {key}: {val}")
+    return '{\n' + '\n'.join(result) + '\n}'
